77319c01b5eb3a8c1e13d84d7b50660c3f390e6d
Kimberly Tze Wey Wong <61455476+wong0825@users.noreply.github.com>
2020-06-05 16:31:44 +0930

    final

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.LLAW33012020S1P12',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# P12',
+      long_description='# P12\r\nCode for Hutt Street Centre - NDIS Navigator \r\n\r\nThis application is created for Hutt Street Centre for the purpose of assisting staff members of the Hutt Street Centre in assessing the eligibility of those seeking to apply for the NDIS and guide them through a disability assessment (which will be the WHODAS assessment). This application will work as an online questionnaire, compiling data from the answered questions into an easy-to-read information sheet for the Hutt Street Centre staff to view. This information sheet will hold no legal value but will be of extreme value in having a more rigorous assessment for clients who have disabilities, by providing a formal and standardised process for staff members to follow and then determine the clients’ eligibility for the NDIS. The information sheet can also be used as a supporting evidence in clients’ NDIS application. By creating a simple questionnaire application, the Hutt Street Centre staff members will be able to reach a decision as to whether a client may or may not be eligible for the NDIS. \r\n',
       long_description_content_type='text/markdown',
       author='Mark Ferraretto',
       author_email='mark.ferraretto@flinders.edu.au',
